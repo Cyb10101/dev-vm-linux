5ec9fd3901b5a65d6603284ad540a93dfb7487b3
Cyb10101 <cyb10101@gmail.com>
2018-08-05 14:22:50 +0200

    [BUGFIX] Better method to start php fpm added

--- home/user/.start-php-fpm.sh
+++ home/user/.start-php-fpm.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+
 function startPhpFpm {
-	if [ -f /home/user/.phpbrew/php/php-${1}/bin/php ]; then
-		phpbrew use ${1}
-		phpbrew fpm start
+	_PHP_VERSION=php-${1}
+	if [ -f ${PHPBREW_ROOT}/php/${_PHP_VERSION}/bin/php ]; then
+		mkdir -p ${PHPBREW_ROOT}/php/${_PHP_VERSION}/var/run
+		PHPFPM_BIN=${PHPBREW_ROOT}/php/${_PHP_VERSION}/sbin/php-fpm
+		PHPFPM_PIDFILE=${PHPBREW_ROOT}/php/${_PHP_VERSION}/var/run/php-fpm.pid
+
+		${PHPFPM_BIN} --php-ini ${PHPBREW_ROOT}/php/${_PHP_VERSION}/etc/php.ini \
+			--fpm-config ${PHPBREW_ROOT}/php/${_PHP_VERSION}/etc/php-fpm.conf \
+			--pid ${PHPFPM_PIDFILE}
 	fi
 }
 
